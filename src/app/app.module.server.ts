c939ce05ccfd27e9d69052a5179676d9a448aca6
Tanvir Hasan Prince <69748320+TanvirHasanPrince@users.noreply.github.com>
2024-04-28 16:14:00 +0600

    Chapter-6

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
-
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+    }),
   ],
   bootstrap: [AppComponent],
 })
